64c650e49543451abf7f07f598a9f0e533977379
Diamond Bond <diamond.nigel75@Gmail.com>
2015-06-06 18:08:19 +0530

    ifs

--- sync.sh
+++ sync.sh
@@ -1,22 +1,35 @@
 # HybridMod sync script v1.0
 
+#Variables
+setup=0
+
+
 #Directories to be synced
 legacy(){
-  git clone https://github.com/HybridMod/Legacy
+  if [ setup = 1 ]; then
+    git clone https://github.com/HybridMod/Legacy
+  else
+  
   cd Legacy
   git pull
   cd ..
 }
 
 modules(){
-  git clone https://github.com/HybridMod/Modules
+  if [ setup = 1 ]; then
+    git clone https://github.com/HybridMod/Modules
+  else
+  
   cd Modules
   git pull
   cd ..
 }
 
 current(){
-  git clone https://github.com/HybridMod/Current
+  if [ setup = 1 ]; then
+    git clone https://github.com/HybridMod/Current
+  else
+  
   cd Current
   git pull
   cd ..
