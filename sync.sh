6f7ce82bac8165eb442333c6d0f0f2d1bb7125fa
Diamond Bond <diamond.nigel75@Gmail.com>
2015-06-06 17:56:05 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,30 @@
-#temp
+# HybridMod sync script v1.0
+
+#Directories to be synced
+legacy(){
+  cd legacy
+  git pull
+  cd ..
+}
+
+modules(){
+  cd modules
+  git pull
+  cd ..
+}
+
+current(){
+  cd current
+  git pull
+  cd ..
+}
+
+#session behaviour
+cd ..
+#sync queue
+sync(){
+  legacy
+  modules
+  current
+} && sync
+clear
