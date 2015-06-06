7cf4388576953f7876cc908198d6a7dbe239931f
Diamond Bond <diamond.nigel75@Gmail.com>
2015-06-06 18:43:21 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,26 @@
+#!/bin/bash
 # Sync Script
+
+# Colors
+red=$(tput setaf 1)
+grn=$(tput setaf 2)
+ylw=$(tput setaf 3)
+blu=$(tput setaf 4)
+ppl=$(tput setaf 5)
+cya=$(tput setaf 6)
+txtbld=$(tput bold)
+bldred=${txtbld}$(tput setaf 1)
+bldgrn=${txtbld}$(tput setaf 2)
+bldylw=${txtbld}$(tput setaf 3)
+bldblu=${txtbld}$(tput setaf 4)
+bldppl=${txtbld}$(tput setaf 5)
+bldcya=${txtbld}$(tput setaf 6)
+txtrst=$(tput sgr0)
+rev=$(tput rev)
+pplrev=${rev}$(tput setaf 5)
+cyarev=${rev}$(tput setaf 6)
+ylwrev=${rev}$(tput setaf 3)
+blurev=${rev}$(tput setaf 4)
 
 #variables
 setup=0
@@ -8,7 +30,7 @@ if [ $setup == 1 ]; then
   git clone https://github.com/HybridMod/Legacy
 fi
 cd Legacy
-echo -n "Legacy: " && git pull
+echo ${txtbld}"Legacy: "${txtrst} && git pull
 cd ..
 }
 
@@ -17,7 +39,7 @@ if [ $setup == 1 ]; then
   git clone https://github.com/HybridMod/Modules
 fi
 cd Modules
-echo -n "Modules: " && git pull
+echo ${txtbld}"Modules: "${txtrst} && git pull
 cd ..
 }
 
@@ -26,7 +48,7 @@ if [ $setup == 1 ]; then
   git clone https://github.com/HybridMod/Current
 fi
 cd Current
-echo -n "Current: " && git pull
+echo ${txtbld}"Current: "${txtrst} && git pull
 cd ..
 }
 
