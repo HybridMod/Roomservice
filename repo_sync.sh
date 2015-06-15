5c37e861bcda9d99dadfe72e20a696423b342466
Diamond Bond <diamond.nigel75@gmail.com>
2015-06-15 12:45:57 +0530

    finished

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,25 +1,34 @@
 #repo_sync.sh created by Diamond Bond | 2015
 
-var(){
-	repotoolver=1.0
-	dirsetup=0
+#tracking
+current=https://github.com/HybridMod/Current
+modules=https://github.com/HybridMod/Modules
+legacy=https://github.com/HybridMod/Legacy
+release=https://github.com/HybridMod/Release
+roomservice=https://github.com/HybridMod/roomservice
+
+#functions
+makedirs(){
+	mkdir -p ~/Scripts/HybridMod
 }
 
-dir_setup(){
-	#temp
+remove(){
+	cd ~/Scripts/HybridMod/
+	rm -rf Current
+	rm -rf Modules
+	rm -rf Legacy
+	rm -rf Release
+}
+
+sync(){
+	cd ~/Scripts/HybridMod/
+	git clone $current
+	git clone $modules
+	git clone $legacy
+	git clone $release
+	# git clone $roomservice
 }
 
 #session_behaviour
-#call functions
-var
-#conditionals
-if [ -d ~/Scripts/HybridMod/ ]; then
-	dirsetup=0
-else
-	dirsetup=1
-fi
-#more functions
-if [ $dirsetup -eq 1 ]; then
-	mkdir -p ~/Scripts/HybridMod/
-installdir="/system/etc/init.d/"
-initd=`if [ -d $initd_dir ]; then echo 1; else echo 0; fi`
+options && tracking
+remove && sync
